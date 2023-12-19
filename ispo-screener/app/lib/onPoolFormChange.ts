fbeb2d7c621c332de8e0532cc78ba3a94bc56229
Dukepq <maikelvdj@live.be>
2023-12-19 15:15:05 +0100

    add pool form change handler

---
+++ ispo-screener/app/lib/onPoolFormChange.ts
@@ -0,0 +1,24 @@
+const numTypes = [
+  "lifetimeBlocks",
+  "lifetimeRewards",
+  "amountInPool",
+  "committedPledge",
+  "activePledge",
+];
+
+export default function onPoolFormChange(
+  e: React.ChangeEvent<HTMLInputElement>,
+  setFields: (prev: React.SetStateAction<Partial<Pool>>) => void
+) {
+  const { value, name } = e.target;
+  if (numTypes.includes(name)) {
+    const valueNum = Number(value);
+    if (isNaN(valueNum)) return;
+    setFields((prev) => {
+      return { ...prev, [name]: valueNum };
+    });
+  }
+  setFields((prev) => {
+    return { ...prev, [name]: value };
+  });
+}
