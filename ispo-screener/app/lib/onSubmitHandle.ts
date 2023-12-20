9e35c5052b7ea7a4c1d717221e96c2d30a45f701
Dukepq <maikelvdj@live.be>
2023-12-20 09:57:06 +0100

    create onSubmitHandle func

---
+++ ispo-screener/app/lib/onSubmitHandle.ts
@@ -0,0 +1,34 @@
+import base from "./routes";
+import toast from "react-hot-toast";
+
+export default async function onSubmitHandle(
+  e: React.FormEvent<HTMLFormElement>,
+  fields: Partial<Pool>
+) {
+  e.preventDefault();
+
+  const response = await fetch(`${base}/api/pools`, {
+    method: "PUT",
+    cache: "no-cache",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      ticker: fields.ticker,
+      pool: fields,
+    }),
+  });
+  if (!response.ok) {
+    toast.error("error", {
+      style: {
+        backgroundColor: "red",
+      },
+    });
+    throw new Error("failed to submit: " + response.status);
+  } else {
+    toast.success("updated", {
+      style: { backgroundColor: "lightgreen" },
+    });
+  }
+}
