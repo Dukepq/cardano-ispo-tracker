0829e9a1a8ff1c5e2a281b3df77a6c7e0c026766
Dukepq <maikelvdj@live.be>
2023-12-25 22:02:44 +0100

    refactor: remove toast notification triggers

--- ispo-screener/app/lib/deletePool.ts
+++ ispo-screener/app/lib/deletePool.ts
@@ -1,5 +1,4 @@
 import base from "./routes";
-import toast from "react-hot-toast";
 
 export default async function (ticker: string) {
   const response = await fetch(base + "/api/pools", {
@@ -12,19 +11,7 @@ export default async function (ticker: string) {
     body: JSON.stringify({ ticker }),
   });
   if (!response.ok) {
-    toast.error("could not delete pool", {
-      style: {
-        backgroundColor: "red",
-        color: "white",
-      },
-    });
     throw new Error("failed to delete pool: " + response.status);
-  } else {
-    toast.success("deleted pool", {
-      style: {
-        backgroundColor: "lightgreen",
-      },
-    });
   }
   return response;
 }
