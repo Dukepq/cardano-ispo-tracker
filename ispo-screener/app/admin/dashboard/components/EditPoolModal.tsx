a4a5edbadc90d601d3db4ec5703989b467c19714
Dukepq <maikelvdj@live.be>
2023-12-25 22:08:59 +0100

    refactor: minor changes

--- ispo-screener/app/admin/dashboard/components/EditPoolModal.tsx
+++ ispo-screener/app/admin/dashboard/components/EditPoolModal.tsx
@@ -6,9 +6,10 @@ import styles from "./project-dialog.module.css";
 import Dropdown from "./PoolSelectDropdown";
 import fetchPools from "@/app/lib/fetchPool";
 import EditPoolForm from "./EditPoolForm";
-import Loading from "@/app/(main)/loading";
 import deletePool from "@/app/lib/deletePool";
 import onSubmitHandle from "@/app/lib/onSubmitHandle";
+import Spinner from "@/app/components/Spinner";
+import AlertDialogWindow from "./AlertDialog";
 
 export default function EditPoolModal({
   children,
@@ -18,9 +19,9 @@ export default function EditPoolModal({
   poolOf: string;
 }) {
   const [fields, setFields] = useState<Partial<Pool>>({});
+  const [targetPool, setTargetPool] = useState<Pool | null>(null);
   const [open, setOpen] = useState(false);
   const [pools, setPools] = useState<Pool[]>([]);
-  const [targetPool, setTargetPool] = useState<Pool | null>(null);
   const [isUpdating, setIsUpdating] = useState(false);
   const onClickHandle = async () => {
     const pools = await fetchPools(poolOf);
@@ -30,7 +31,6 @@ export default function EditPoolModal({
   useEffect(() => {
     if (targetPool) {
       setFields(() => {
-        console.log(targetPool);
         return { ...targetPool };
       });
     }
@@ -56,7 +56,7 @@ export default function EditPoolModal({
       <Dialog.Portal>
         <Dialog.Overlay className={styles["dialog-overlay"]} />
         <Dialog.Content className={styles["dialog-content"]}>
-          {isUpdating && <Loading css={{ position: "absolute", inset: 0 }} />}
+          {isUpdating && <Spinner css={{ position: "absolute", inset: 0 }} />}
           <Dialog.Title className={styles.title}>
             <p>Edit existing pools</p>
             <Dialog.Close className={styles.button}>close</Dialog.Close>
@@ -68,21 +68,6 @@ export default function EditPoolModal({
           >
             {pools.map((pool) => pool.ticker)}
           </Dropdown>
-          {targetPool && (
-            <button
-              onClick={() => {
-                console.log(targetPool.ticker);
-                try {
-                  deletePool(targetPool.ticker);
-                  setOpen(false);
-                } catch (err) {
-                  console.error(err);
-                }
-              }}
-            >
-              delete pool
-            </button>
-          )}
           {targetPool && (
             <>
               <EditPoolForm
@@ -99,7 +84,20 @@ export default function EditPoolModal({
                     console.error(err);
                   }
                 }}
-              />
+              >
+                <AlertDialogWindow
+                  deleteFunc={async () => {
+                    const res = await deletePool(targetPool.ticker);
+                    setOpen(false);
+                    return res;
+                  }}
+                  arg={targetPool.ticker}
+                >
+                  <button className={styles["delete-button"]}>
+                    delete pool
+                  </button>
+                </AlertDialogWindow>
+              </EditPoolForm>
             </>
           )}
         </Dialog.Content>
