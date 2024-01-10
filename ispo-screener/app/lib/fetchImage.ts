042f68ab4e1cc257b89c9cb682c0c6f07968292c
Dukepq <maikelvdj@live.be>
2024-01-11 00:41:45 +0100

    feat: allow user to attach images to a project

---
+++ ispo-screener/app/lib/fetchImage.ts
@@ -0,0 +1,16 @@
+import base from "./routes";
+
+export default async function fetchImage(path: string): Promise<Blob> {
+  const response = await fetch(base + "/api/projects/logo", {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      path,
+    }),
+  });
+  if (!response.ok) throw new Error(response.status + " could not fetch image");
+  return response.blob();
+}
