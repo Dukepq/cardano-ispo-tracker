e57a0bd2892824f7f944f78d2e0bfd840b00d396
Dukepq <maikelvdj@live.be>
2024-02-26 02:59:18 +0100

    style: search bar style changes

--- ispo-screener/app/admin/dashboard/components/SearchBar.tsx
+++ ispo-screener/app/admin/dashboard/components/SearchBar.tsx
@@ -3,15 +3,18 @@
 import { useRef } from "react";
 import styles from "./table.module.css";
 import debounce from "@/app/lib/debounce";
+import { FileX, Search as SearchIcon } from "lucide-react";
 
 type SearchBarProps = {
   setState: (prev: React.SetStateAction<string>) => void;
   setIsDebouncing?: (prev: React.SetStateAction<boolean>) => void;
+  hideIcon?: boolean;
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
 export default function SearchBar({
   setState,
   setIsDebouncing,
+  hideIcon = false,
   ...attributes
 }: SearchBarProps) {
   const searchRef = useRef<HTMLInputElement | null>(null);
@@ -23,17 +26,33 @@ export default function SearchBar({
     300
   );
   return (
-    <input
-      type="text"
-      ref={searchRef}
-      onKeyDown={(e) => {
-        if (e.code === "Enter") {
-          setState(() => searchRef.current?.value || "");
-        }
-      }}
-      onChange={async (e) => onChangeDebounce(e)}
-      className={styles["search-input"]}
-      {...attributes}
-    />
+    <div style={{ position: "relative", display: "flex" }}>
+      <input
+        type="text"
+        ref={searchRef}
+        onKeyDown={(e) => {
+          if (e.code === "Enter") {
+            setState(() => searchRef.current?.value || "");
+          }
+        }}
+        onChange={async (e) => onChangeDebounce(e)}
+        className={styles["search-input"]}
+        style={hideIcon ? {} : { paddingLeft: "2rem" }}
+        {...attributes}
+      />
+      {!hideIcon && (
+        <SearchIcon
+          width={15}
+          height={15}
+          style={{
+            position: "absolute",
+            left: "8px",
+            display: "inline-block",
+            margin: "auto 0",
+            alignSelf: "center",
+          }}
+        />
+      )}
+    </div>
   );
 }
--- ispo-screener/app/components/GradientBlobs.tsx
+++ ispo-screener/app/components/GradientBlobs.tsx
@@ -15,12 +15,12 @@ type GradientBlobsProps = {
 
 export default function GradientBlobs({
   children,
-  firstColor = "0, 0, 180",
-  secondColor = "0, 0, 180",
-  thirdColor = "80, 80, 180",
-  fourthColor = "0, 0, 180",
-  fifthColor = "0, 0, 180",
-  sixthColor = "0, 0, 180",
+  firstColor = "30, 30, 180",
+  secondColor = "30, 30, 180",
+  thirdColor = "30, 30, 180",
+  fourthColor = "80, 0, 180",
+  fifthColor = "30, 30, 180",
+  sixthColor = "30, 30, 180",
   blobSize = "calc(25vw + 1100px)",
   firstBackgroundColor,
   secondBackgroundColor,
@@ -45,7 +45,7 @@ export default function GradientBlobs({
           </filter>
         </defs>
       </svg>
-      <div style={{ backdropFilter: "blur(38px)" }}>{children}</div>
+      <div style={{ backdropFilter: "blur(180px)" }}>{children}</div>
       <div
         className={styles["gradients-container"]}
         style={
