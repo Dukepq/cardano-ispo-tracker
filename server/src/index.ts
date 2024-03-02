1e9cc2c3b4ddfca70ac67760f52f563af38e762c
Dukepq <maikelvdj@live.be>
2024-03-02 05:36:44 +0100

    /

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ import sessionMiddleware from "./config/session-config";
 import envHelper from "./utils/envHelper";
 const cors = require("cors");
 
-app.set("trust proxy", 2);
+app.set("trust proxy", 1);
 
 const entryDir = __dirname;
 export { entryDir };
