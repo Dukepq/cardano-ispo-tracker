5f8c713ba13d24d620f544c330a8ab5b39a1e7ad
Dukepq <maikelvdj@live.be>
2024-03-02 04:36:04 +0100

    /

--- ispo-screener/app/admin/dashboard/page.tsx
+++ ispo-screener/app/admin/dashboard/page.tsx
@@ -8,7 +8,6 @@ import PageHeader from "./components/PageHeader";
 
 export default async function Dashboard() {
   const cookie = headers().get("cookie");
-  console.log(cookie);
   if (typeof cookie !== "string") {
     redirect("/admin");
   }
