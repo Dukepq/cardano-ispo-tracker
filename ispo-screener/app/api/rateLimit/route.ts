86b243eda31ec85c6e5d394109e0ba9c9ed735fb
Dukepq <maikelvdj@live.be>
2024-03-04 01:38:33 +0100

    fix: headers

--- ispo-screener/app/api/rateLimit/route.ts
+++ ispo-screener/app/api/rateLimit/route.ts
@@ -3,7 +3,6 @@ import userLimit from "../limiter";
 export async function GET(request: Request) {
   const forwarded = request.headers.get("X-Forwarded-For") ?? "127.0.0.1";
   const clientIp = forwarded.split(",")[0];
-  console.log("API IP: " + clientIp);
   const remainingLimit = await userLimit(clientIp, 1);
   const headers = new Headers({
     "X-RateLimit-Limit": process.env.NEXT_SERVER_RATELIMIT!,
--- ispo-screener/middleware.ts
+++ ispo-screener/middleware.ts
@@ -3,7 +3,6 @@ import { nextBase } from "./app/lib/routes";
 
 export async function middleware(request: NextRequest) {
   const ip = request.headers.get("X-Forwarded-For") ?? "127.0.0.1";
-  console.log("middleware IP: " + ip);
   const response = await fetch(nextBase + "/api/rateLimit", {
     headers: {
       "X-Forwarded-For": ip,
