276c7457af0b906b50ab9721f0f84b5ea36219c5
Dukepq <maikelvdj@live.be>
2024-03-04 01:13:02 +0100

    /

--- ispo-screener/middleware.ts
+++ ispo-screener/middleware.ts
@@ -4,7 +4,11 @@ import { nextBase } from "./app/lib/routes";
 export async function middleware(request: NextRequest) {
   const ip = request.headers.get("X-Forwarded-For") ?? "127.0.0.1";
   console.log("middleware IP: " + ip);
-  const response = await fetch(nextBase + "/api/rateLimit", {});
+  const response = await fetch(nextBase + "/api/rateLimit", {
+    headers: {
+      "X-Forwarded-For": ip,
+    },
+  });
   if (response.status === 429) {
     return NextResponse.json({ error: "Too many requests" });
   }
